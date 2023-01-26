0a0e5ba0337e3e97a27a9165381da8dfc9bee981
Thiago <pereirathi886@gmail.com>
2023-01-26 19:12:19 -0300

    change CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://thiago-pereira.dev/',
+    site: 'http://thiago-pereira.dev/',
 });
