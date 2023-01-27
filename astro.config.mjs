83b67175f6590377a18e50488df02236ae2d08f1
Thiago <pereirathi886@gmail.com>
2023-01-27 17:33:34 -0300

    added google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'http://thiago-pereira.dev/',
+  site: 'http://thiago-pereira.dev/',
+  integrations: [partytown()]
 });
